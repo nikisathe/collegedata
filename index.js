0566ea0a6c143ff36b93b3702d97c9cc48ef4d26
nikisathe <155984881+nikisathe@users.noreply.github.com>
2024-04-27 15:56:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const college_connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT, 
-});
+ });
 
 college_connection.connect((err) => {
   if (err) {
